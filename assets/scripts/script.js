4c047fd580b849f32bf794ba39cbb336d95950e7
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2025-04-09 16:24:32 +0300

    Refactor project structure

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,38 +1,3 @@
-const slides = [
-    {
-        src: '/assets/images/tech_camera.jpg',
-        alt: 'tech-camera',
-    },
-    {
-        src: '/assets/images/tech_drone.jpg',
-        alt: 'tech-drone',
-    },
-    {
-        src: '/assets/images/tech_mic.jpg',
-        alt: 'tech-mic',
-    },
-    {
-        src: '/assets/images/tech_tablet.jpg',
-        alt: 'tech-tablet',
-    },
-    {
-        src: '/assets/images/tech_phone.jpg',
-        alt: 'tech-phone',
-    },
-    {
-        src: '/assets/images/tech_sound.jpg',
-        alt: 'tech-sound',
-    },
-    {
-        src: '/assets/images/tech_tableturner.jpg',
-        alt: 'tech-tableturner',
-    },
-    {
-        src: '/assets/images/tech_typewriter.jpg',
-        alt: 'tech-typewriter',
-    }
-];
-
 let countIndex = 0;
 let countIndBack = slides.length - 1;
 let countIndNext = 1;
